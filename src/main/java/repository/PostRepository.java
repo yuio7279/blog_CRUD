48ceee1b6cba3bfc3ec30adf539fbf38d46c330c
yuio7279 <yuio7279@gmail.com>
2023-06-15 13:16:05 +0900

    fix save  (memory Repository)

--- src/main/java/repository/PostRepository.java
+++ src/main/java/repository/PostRepository.java
@@ -5,6 +5,7 @@ import dto.PostResponseDto;
 import entity.Post;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,11 +16,10 @@ public class PostRepository {
     Map<Long, Post> store = new HashMap<>();
 
     public PostResponseDto save(Post post){
-        if(store.size() > 0){
-            store.put(post.getId()+1L,post);
-        }else{
-            store.put(1L,post);
-        }
+
+        Long maxId = store.size() > 0? Collections.max(store.keySet())+1 : 1;
+        post.setId(maxId);
+        store.put(post.getId(),post);
         //dto 변환
         PostResponseDto postResponseDto = new PostResponseDto(post);
 
